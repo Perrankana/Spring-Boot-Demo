fa6dee8450c6cf548f94f408ae3451f735879361
Rocio Ortega <rocioortegapriego@gmail.com>
2019-07-12 18:23:42 +0200

    Add Persistance and configure blog engine

---
+++ blog/src/main/kotlin/com/example/blog/repositories/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.example.blog.repositories
+
+import com.example.blog.entities.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<User, Long> {
+    fun findByLogin(login: String): User
+}
