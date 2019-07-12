8561967f9c98386fe075e2bf07c845baa3702693
Rocio Ortega <rocioortegapriego@gmail.com>
2019-07-12 18:26:54 +0200

    Exposing HTTP API WIP

--- blog/src/main/kotlin/com/example/blog/controllers/HtmlController.kt
+++ blog/src/main/kotlin/com/example/blog/controllers/HtmlController.kt
@@ -4,11 +4,14 @@ import com.example.blog.entities.Article
 import com.example.blog.entities.User
 import com.example.blog.repositories.ArticleRepository
 import com.example.blog.extensions.format
+import com.example.blog.repositories.UserRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class HtmlController(private val repository: ArticleRepository) {
@@ -28,6 +31,30 @@ class HtmlController(private val repository: ArticleRepository) {
         model["title"] = article.title
         model["article"] = article
         return "article"
+    }
+
+    @RestController
+    @RequestMapping("/api/article")
+    class ArticleController(private val repository: ArticleRepository) {
+
+        @GetMapping("/")
+        fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+        @GetMapping("/{slug}")
+        fun findOne(@PathVariable slug: String) =
+                repository.findBySlug(slug) ?: throw IllegalArgumentException("Wrong article slug provided")
+
+    }
+
+    @RestController
+    @RequestMapping("/api/user")
+    class UserController(private val repository: UserRepository) {
+
+        @GetMapping("/")
+        fun findAll() = repository.findAll()
+
+        @GetMapping("/{login}")
+        fun findOne(@PathVariable login: String) = repository.findByLogin(login)
     }
 
     fun Article.render() = RenderedArticle(
